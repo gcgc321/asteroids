d5348b02f0be3fa022474f5341136d6c4816ed1e
gcgc321 <gcgc321@gmail.com>
2025-08-16 14:36:04 +0200

    physics

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from textwrap import fill
 import pygame
 from constants import *
+from player import Player
+import player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width:",SCREEN_WIDTH)
-    print(f"Screen height:",SCREEN_HEIGHT)
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    clock = pygame.time.Clock  
+    dt = 0
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
         screen.fill(color = "black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
+        #limit framerate to 60fps
+        dt = clock.tick(60) / 1000
         
         
 
